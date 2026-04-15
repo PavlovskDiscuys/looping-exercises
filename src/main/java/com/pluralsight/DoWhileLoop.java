f9e13ee69bba10aea6cbe5718fd9c24daa08513a
Pav <robsarmiento@icloud.com>
2026-04-15 11:48:25 -0500

    Step 1 Fully Complete

--- src/main/java/com/pluralsight/DoWhileLoop.java
+++ src/main/java/com/pluralsight/DoWhileLoop.java
@@ -1,4 +1,22 @@
 package com.pluralsight;
 
 public class DoWhileLoop {
+    public static void main(String[] args) {
+
+        //  execute body → check condition → repeat if true. Guarantees at least one execution.
+
+        //  Step 1
+
+        // initial counter variable
+        int count = 1;
+        int maxCount = 5;
+
+        //  Condition
+        do {
+            System.out.println("I love Java");
+            count = count + 1;
+        }   while (count <= maxCount);
+
+        // ----------------------------------------------
+    }
 }
--- src/main/java/com/pluralsight/WhileLoop.java
+++ src/main/java/com/pluralsight/WhileLoop.java
@@ -1,4 +1,24 @@
 package com.pluralsight;
 
 public class WhileLoop {
+    public static void main(String[] args) {
+
+        //  check condition → execute body only if condition true. May execute zero times.
+
+        //  Step 1
+
+        // initial counter variable
+        int count = 1;
+        int maxCount = 5;
+
+        //  Condition
+        while (count <= maxCount) {
+            count = count + 1;
+            System.out.println("I love Java");
+        }
+
+        // ----------------------------------------------
+
+
+    }
 }
