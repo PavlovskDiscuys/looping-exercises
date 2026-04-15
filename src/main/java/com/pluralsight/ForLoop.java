1b16121e62af9d93ba50c02d2557c51a0e21dd27
Pav <robsarmiento@icloud.com>
2026-04-15 11:55:20 -0500

    Step 2 Fully Complete

---
+++ src/main/java/com/pluralsight/ForLoop.java
@@ -0,0 +1,20 @@
+package com.pluralsight;
+
+public class ForLoop {
+    public static void main(String[] args) throws InterruptedException {
+
+        //
+
+        //  Step 1
+
+        // initial counter variable
+        //  Condition
+        for (int count = 10;count >= 1; count--) {
+            System.out.println(count);
+            Thread.sleep(1000);
+        }
+        System.out.println("Launch");
+
+
+    }
+}
